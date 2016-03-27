0d1b5aa0f202f1f223efe1efccf58e0ec6760d65
Duong <duongnartist@gmail.com>
2016-03-27 13:55:00 +0700

    demo app

---
+++ src/edu/epu/sentiment/analysis/utils/Log.java
@@ -0,0 +1,16 @@
+package edu.epu.sentiment.analysis.utils;
+
+/**
+ * Created by duong on 3/27/16.
+ */
+public class Log {
+
+    public static void prln(String msg) {
+        System.out.println(msg);
+    }
+
+    public static void pr(String msg) {
+        System.out.print(msg);
+    }
+
+}
